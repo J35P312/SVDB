6b4c495297486bf9c10e2debf6b2da40ee732288
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-07-04 14:05:42 +0200

    deleted:    SVDB_build_module_cython.pyx 	deleted:    SVDB_merge_vcf_module_cython.pyx 	deleted:    SVDB_overlap_module.pyx 	deleted:    readVCF.pyx 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(["SVDB_overlap_module.pyx","SVDB_build_module_cython.pyx","readVCF.pyx","SVDB_merge_vcf_module_cython.pyx"])
+    ext_modules = cythonize(["SVDB_overlap_module.py","SVDB_build_module_cython.py","readVCF.py","SVDB_merge_vcf_module_cython.py"])
 )
