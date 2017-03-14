e491b89e216c59ef61301bee78a3bd04c087483e
J35P312 <jesper.eisfeldt@scilifelab.se>
2017-03-14 15:17:00 +0100

    modified:   svdb/merge_vcf_module.py

--- svdb/merge_vcf_module.py
+++ svdb/merge_vcf_module.py
@@ -1,14 +1,17 @@
 from __future__ import absolute_import
 from . import readVCF
 from . import merge_vcf_module_cython
+import sys
 
-def print_header(vcf_list,vcf_dictionary,args):
+def print_header(vcf_list,vcf_dictionary,args,command_line):
     header={};
     header["ALT"]={}
     header["INFO"]={}
     header["FILTER"]={}
     header["FORMAT"]={}
-
+    header["CONTIGS"]=[]
+    contigs=False
+    reference=""
     subheader={}
     columns=["#CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO"]
     sample_ids=set([])
@@ -28,24 +31,33 @@ def print_header(vcf_list,vcf_dictionary,args):
                                 sample_order[column.strip()]={}
                                 samples.append(column.strip())
 
-                    if len(vcf_columns) > 8:
+                    if len(vcf_columns) > 8 and not args.same_order:
                         i=0
-                        for sample in vcf_columns[9:]:                                
-                            sample_order[sample][vcf_dictionary[vcf]]=i
-                            i += 1
-
+                        for sample in vcf_columns[9:]:
+                            try:                            
+                                sample_order[sample][vcf_dictionary[vcf]]=i
+                                i += 1
+                            except:
+                                print "sample mismatch! check the samples of the vcf files, or use --same_order"
+                                quit()
                 elif line[0] == line[1] and "=" in line:
-                    if("ID=" in line):
+                    if("ID=" in line and not "##contig=<ID=" in line):
                         field=line.split("=")[2].split(",")[0]
                         key= line.strip("#").split("=")[0]
                         if not key in header:
                             header[key]={}
                         header[key][field]=line
+                    elif "##contig=<ID=" in line and not contigs:
+                        header["CONTIGS"].append(line)
+                    elif "##reference=" in line and not contigs:
+                        reference=line
                     elif not "##source=" in line and not "##file" in line and not "##reference=" in line:
                         key=line.strip("#").split("=")[0]
                         if not key in subheader:
                                 subheader[key]=line
             else:
+                if header["CONTIGS"]:
+                    contigs = True
                 break
     #print the mandatory header lines in the correct order
     for entry in sorted(header["ALT"]):
@@ -54,6 +66,12 @@ def print_header(vcf_list,vcf_dictionary,args):
     for entry in sorted(header["INFO"]):
         print(header["INFO"][entry].strip())
     del header["INFO"]
+    #print contigs according to the input order
+    if reference != "":
+        print reference.strip()
+    for entry in header["CONTIGS"]:
+        print(entry.strip())
+    del header["CONTIGS"]
     for entry in sorted(header["FILTER"]):
         print(header["FILTER"][entry].strip())
     del header["FILTER"]
@@ -75,6 +93,7 @@ def print_header(vcf_list,vcf_dictionary,args):
     for entry in sorted(subheader):
         print(subheader[entry].strip())
     print("##INFO=<ID=VARID,Number=1,Type=String,Description=\"The variant ID of merged samples\">")
+    print("##svdbcmdline={}".format(" ".join(command_line)))
     if sample_ids:
         sorted_samples=sorted(list(sample_ids))
         print( "\t".join(columns+sorted_samples) )
@@ -128,7 +147,7 @@ def main(args):
                         variants[chrA].append([chrB,event_type,posA,posB,vcf,i,line.strip()])
                     i+=1;
 
-    samples,sample_order=print_header(vcf_list,vcf_dictionary,args)
+    samples,sample_order=print_header(vcf_list,vcf_dictionary,args,sys.argv)
 
     to_be_printed=merge_vcf_module_cython.merge(variants,samples,sample_order,priority_order,args)
     #print the variants in similar order as the input
