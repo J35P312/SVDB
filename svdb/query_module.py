c29f61a81608f79c0d867ab3b1d9de02914ac3d1
Daniel T. Andreasen <daniel.andreasen@hotmail.com>
2020-11-24 15:54:25 +0100

    Cleaning and refactoring slightly

--- svdb/query_module.py
+++ svdb/query_module.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
 import gzip
-import sqlite3
 import sys
 
 import numpy as np
+
+from db import DB
 
 from . import overlap_module, readVCF
 
@@ -48,8 +49,7 @@ def main(args):
             # in this case I need to store a query
             chrA, posA, chrB, posB, event_type, INFO, FORMAT = readVCF.readVCFLine(line)
             # plus a counter and the variation
-            current_variation = [chrA, int(posA), chrB, int(posB), event_type, FORMAT, line]
-            queries.append(current_variation)
+            queries.append([chrA, int(posA), chrB, int(posB), event_type, FORMAT, line])
 
     # at this point queries contains an entry for each variation
     # now query each sample.db present in the given folder and store the occurences
@@ -60,80 +60,71 @@ def main(args):
         db_file = args.db
         DBvariants = {}
         db_size = 1
-        Use_OCC_tag = False
+        use_OCC_tag = False
         if args.in_occ:
             OCC_tag = args.in_occ
-            Use_OCC_tag = True
+            use_OCC_tag = True
 
         if args.in_frq:
             FRQ_tag = args.in_frq
 
-        if db_file.endswith(".gz"):
-            f = gzip.open(db_file, "rt")
-        else:
-            f = open(db_file, "rt")
-
+        opener = gzip.open if db_file.endswith(".gz") else open
         # print FRQ_tag
-        for line in f:
-            if line[0] == "#":
-                continue
+        with opener(db_file, 'rt') as lines:
+            for line in lines:
+                if line.startswith('#'):
+                    continue
 
-            if args.bedpedb:
-                content = line.strip().split()
+                if args.bedpedb:
+                    content = line.strip().split()
 
-                if (content[0] == content[2] and (int(content[1]) < int(content[3]))) or (content[0] < content[2]):
-                    chrA = content[0]
-                    posA = int(content[1])
-                    chrB = content[2]
-                    posB = int(content[3])
+                    if (content[0] == content[2] and (int(content[1]) < int(content[3]))) or (content[0] < content[2]):
+                        chrA = content[0]
+                        posA = int(content[1])
+                        chrB = content[2]
+                        posB = int(content[3])
+                    else:
+                        chrA = content[2]
+                        posA = int(content[3])
+                        chrB = content[0]
+                        posB = int(content[1])
+
+                    event_type = content[4]
+                    hits = int(content[5])
+                    frequency = float(content[6])
+                    FORMAT = [False]
+
                 else:
-                    chrA = content[2]
-                    posA = int(content[3])
-                    chrB = content[0]
-                    posB = int(content[1])
+                    chrA, posA, chrB, posB, event_type, INFO, FORMAT = readVCF.readVCFLine(line)
 
-                event_type = content[4]
-                hits = int(content[5])
-                frequency = float(content[6])
-                FORMAT = [False]
+                if chrA not in DBvariants:
+                    DBvariants[chrA] = {}
+                if chrB not in DBvariants[chrA]:
+                    DBvariants[chrA][chrB] = {}
+                if event_type not in DBvariants[chrA][chrB]:
+                    DBvariants[chrA][chrB][event_type] = {}
+                    DBvariants[chrA][chrB][event_type]["samples"] = []
+                    DBvariants[chrA][chrB][event_type]["coordinates"] = []
 
-            else:
-                chrA, posA, chrB, posB, event_type, INFO, FORMAT = readVCF.readVCFLine(
-                    line)
+                DBvariants[chrA][chrB][event_type]["coordinates"].append(np.array([int(posA), int(posB)]))
+                if "GT" in FORMAT and not use_OCC_tag:
+                    DBvariants[chrA][chrB][event_type]["samples"].append(np.array(FORMAT["GT"]))
+                    db_size = len(FORMAT["GT"])
+                elif args.bedpedb:
+                    DBvariants[chrA][chrB][event_type]["samples"].append([hits, frequency])
+                    use_OCC_tag = True
 
-            if chrA not in DBvariants:
-                DBvariants[chrA] = {}
-            if chrB not in DBvariants[chrA]:
-                DBvariants[chrA][chrB] = {}
-            if event_type not in DBvariants[chrA][chrB]:
-                DBvariants[chrA][chrB][event_type] = {}
-                DBvariants[chrA][chrB][event_type]["samples"] = []
-                DBvariants[chrA][chrB][event_type]["coordinates"] = []
-
-            DBvariants[chrA][chrB][event_type]["coordinates"].append(
-                np.array([int(posA), int(posB)]))
-            if "GT" in FORMAT and not Use_OCC_tag:
-                DBvariants[chrA][chrB][event_type]["samples"].append(
-                    np.array(FORMAT["GT"]))
-                db_size = len(FORMAT["GT"])
-            elif args.bedpedb:
-                DBvariants[chrA][chrB][event_type]["samples"].append(
-                    [hits, frequency])
-                Use_OCC_tag = True
-
-            else:
-                try:
-                    OCC = INFO[OCC_tag]
-                    FRQ = INFO[FRQ_tag]
-                    DBvariants[chrA][chrB][event_type]["samples"].append([OCC, FRQ])
-                    Use_OCC_tag = True
-                except KeyError:
-                    print("Error: frequency or hit tag not found! Make sure to set the --in_occ AND --in_frq to the number and frequency of alleles/individuals as presented in the INFO column of the input db\n")
-                    print(
-                        "database variants not having the --in_occ or --in_frq tag must be removed")
-                    print(
-                        "you may also skip these parameters and cluster based on the GT entry of the format column (if such exists)")
-                    quit()
+                else:
+                    try:
+                        OCC = INFO[OCC_tag]
+                        FRQ = INFO[FRQ_tag]
+                        DBvariants[chrA][chrB][event_type]["samples"].append([OCC, FRQ])
+                        use_OCC_tag = True
+                    except KeyError:
+                        print("Error: frequency or hit tag not found! Make sure to set the --in_occ AND --in_frq to the number and frequency of alleles/individuals as presented in the INFO column of the input db\n")
+                        print("database variants not having the --in_occ or --in_frq tag must be removed")
+                        print("you may also skip these parameters and cluster based on the GT entry of the format column (if such exists)")
+                        quit()
 
         for chrA in DBvariants:
             for chrB in DBvariants[chrA]:
@@ -144,48 +135,36 @@ def main(args):
                         DBvariants[chrA][chrB][var]["samples"])
 
         for query in queries:
-            hits = queryVCFDB(DBvariants, query, args, Use_OCC_tag)
+            hits = queryVCFDB(DBvariants, query, args, use_OCC_tag)
             query[5] = hits
 
         for query in queries:
             vcf_entry = query[6].strip()
             content = vcf_entry.split("\t")
-            if not Use_OCC_tag:
+            if not use_OCC_tag:
                 if query[5]:
-                    content[7] = "{};{}={};{}={}".format(
-                        content[7], args.out_occ, query[5], args.out_frq, (query[5] / float(db_size)))
+                    content[7] = "{};{}={};{}={}".format(content[7], args.out_occ, query[5], args.out_frq, (query[5] / float(db_size)))
             else:
                 if query[5][0]:
-                    content[7] = "{};{}={};{}={}".format(
-                        content[7], args.out_occ, int(query[5][0]), args.out_frq, query[5][1])
+                    content[7] = "{};{}={};{}={}".format(content[7], args.out_occ, int(query[5][0]), args.out_frq, query[5][1])
 
             if not args.prefix:
                 print(("\t").join(content))
             else:
                 f.write(("\t").join(content) + "\n")
-        return()
+        return None
 
     elif args.sqdb:
-        db_file = args.sqdb
-        conn = sqlite3.connect(args.sqdb)
+        db = DB(db=args.sqdb, memory=args.memory)
 
-        if args.memory:
-            memory_db = sqlite3.connect(':memory:')
-            db_dump = "".join(line for line in conn.iterdump())
-            memory_db.executescript(db_dump)
-            conn.close()
-            c = memory_db.cursor()
-        else:
-            c = conn.cursor()
-
-        A = 'SELECT DISTINCT sample FROM SVDB'
-        db_size = len(c.execute(A).fetchall())
+        db_size = len(db)
         if not db_size:
+            # TODO: Raise a custom DB exception
             print("error: no samples in the db")
             quit()
 
         for query in queries:
-            hits = SQDB(query, args, c)
+            hits = SQDB(query, args, db)
             query[5] = hits
 
     for query in queries:
@@ -202,23 +181,23 @@ def main(args):
             f.write(("\t").join(content) + "\n")
 
 
-def queryVCFDB(DBvariants, Query_variant, args, Use_OCC_tag):
-    chrA = Query_variant[0]
-    chrApos = Query_variant[1]
-    chrB = Query_variant[2]
-    chrBpos = Query_variant[3]
-    variation_type = Query_variant[4]
+def queryVCFDB(DBvariants, query_variant, args, use_OCC_tag):
+    chrA = query_variant[0]
+    chrApos = query_variant[1]
+    chrB = query_variant[2]
+    chrBpos = query_variant[3]
+    variation_type = query_variant[4]
     samples = set([])
     frequency = []
     occ = []
     similarity = []
     if chrA not in DBvariants:
-        if Use_OCC_tag:
+        if use_OCC_tag:
             return([0, 0])
         else:
             return 0
     if chrB not in DBvariants[chrA]:
-        if Use_OCC_tag:
+        if use_OCC_tag:
             return([0, 0])
         else:
             return 0
@@ -226,11 +205,10 @@ def queryVCFDB(DBvariants, Query_variant, args, Use_OCC_tag):
         if not args.no_var and variation_type != var:
             continue
 
-        # candidates=DBvariants[chrA][chrB][var]["coordinates"][ ( args.bnd_distance >= abs(DBvariants[chrA][chrB][var]["coordinates"][:,0] - chrApos)  ) & ( args.bnd_distance >= abs(DBvariants[chrA][chrB][var]["coordinates"][:,1] - chrBpos)  ) ]
         candidates = np.where((args.bnd_distance >= abs(DBvariants[chrA][chrB][var]["coordinates"][:, 0] - chrApos)) & (
             args.bnd_distance >= abs(DBvariants[chrA][chrB][var]["coordinates"][:, 1] - chrBpos)))
         if not len(candidates[0]) and not args.no_var:
-            if Use_OCC_tag:
+            if use_OCC_tag:
                 return([0, 0])
             else:
                 return 0
@@ -251,7 +229,7 @@ def queryVCFDB(DBvariants, Query_variant, args, Use_OCC_tag):
 
             if match:
                 similarity.append(hit_tmp)
-                if Use_OCC_tag:
+                if use_OCC_tag:
                     occ.append(sample_list[0])
                     frequency.append(sample_list[1])
                 else:
@@ -259,7 +237,7 @@ def queryVCFDB(DBvariants, Query_variant, args, Use_OCC_tag):
                         GT = sample_list[i]
                         if not GT == "0|0" and not GT == "0/0":
                             samples = samples | set([i])
-    if Use_OCC_tag:
+    if use_OCC_tag:
         if occ:
             if not (chrA == chrB):
                 idx = similarity.index(min(similarity))
@@ -274,37 +252,28 @@ def queryVCFDB(DBvariants, Query_variant, args, Use_OCC_tag):
     return hits
 
 
-def SQDB(Query_variant, args, c):
+def SQDB(query_variant, args, db):
     distance = args.bnd_distance
     overlap = args.overlap
-    variant = {}
-    variant["type"] = Query_variant[4]
-    variant["chrA"] = Query_variant[0]
-    variant["chrB"] = Query_variant[2]
-    variant["posA"] = Query_variant[1]
-    variant["ci_A_start"] = 0
-    variant["ci_A_end"] = 0
-    variant["posB"] = Query_variant[3]
-    variant["ci_B_start"] = 0
-    variant["ci_B_end"] = 0
+    variant = {"type": query_variant[4],
+               "chrA": query_variant[0], "posA": query_variant[1], "ci_A_start": 0, "ci_A_end": 0,
+               "chrB": query_variant[2], "posB": query_variant[3], "ci_B_start": 0, "ci_B_end": 0}
 
-    selection = "sample"
-    if variant["chrA"] == variant["chrB"]:
-        selection = "posA, posB, sample"
+    selection = "posA, posB, sample" if variant["chrA"] == variant["chrB"] else "sample"
 
     A = 'SELECT {} FROM SVDB WHERE var == \'{}\' AND chrA == \'{}\' AND chrB == \'{}\' AND posA <= {} AND posA >= {} AND posB <= {} AND posB >= {}'.format(
-        selection, variant["type"], variant["chrA"], variant["chrB"], variant["posA"] + distance, variant["posA"] - distance, variant["posB"] + distance, variant["posB"] - distance)
-    hits = c.execute(A)
+        selection, variant["type"], variant["chrA"], variant["chrB"],
+        variant["posA"] + distance, variant["posA"] - distance,
+        variant["posB"] + distance, variant["posB"] - distance)
+    hits = db.query(A)
 
     match = set([])
     for hit in hits:
         if variant["chrA"] == variant["chrB"]:
-            var = {}
-            var["posA"] = int(hit[0])
-            var["posB"] = int(hit[1])
-            var["index"] = hit[2]
-            similar, _ = overlap_module.isSameVariation(
-                variant["posA"], variant["posB"], var["posA"], var["posB"], overlap, distance)
+            var = {"posA": int(hit[0]), "posB": int(hit[1]), "index": hit[2]}
+            similar, _ = overlap_module.isSameVariation(variant["posA"], variant["posB"],
+                                                        var["posA"], var["posB"], overlap, distance)
+
             if similar:
                 match.add(var["index"])
 
