e4d61f612c6fd0de80804b280cf5085c188fbc39
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2026-07-22 15:44:02 +0200

    Update setup.py
    bumbped the version to 2.12.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
     ext_modules = []
 
 setup(name='svdb',
-      version='2.11.1',
+      version='2.12.0',
       url="https://github.com/J35P312/SVDB",
       author="Jesper Eisfeldt",
       author_email="jesper.eisfeldt@scilifelab.se",
